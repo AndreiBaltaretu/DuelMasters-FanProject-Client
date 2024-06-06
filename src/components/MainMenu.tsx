0c18bb5bed7b2860e600779faf81dc1d6e62a950
Andrei <AndreiBaltaretu@users.noreply.github.com>
2024-06-06 18:24:05 +0300

    Added SignOut functionality
    - Added SignOut button to MainMenu.tsx component
    - Added SignOut functionality

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,4 +1,13 @@
+import {auth} from "../config/firebaseConfig.ts";
+import {signOut} from "firebase/auth";
+
 function MainMenu() {
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            console.log("logged out");
+        })
+    }
+
     return (
         <div className="container align-content-center">
             <div className="d-grid gap-2">
@@ -10,6 +19,11 @@ function MainMenu() {
                 </button>
                 <button className="btn btn-primary h3">
                     All cards database
+                </button>
+                <br/>
+                <br/>
+                <button className="btn btn-primary h3" onClick={handleSignOut}>
+                    Sign out
                 </button>
             </div>
         </div>
